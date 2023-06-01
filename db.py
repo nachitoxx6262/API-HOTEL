7c78f15567b20db281b891620a80b19bf1e0057c
Nacho_Devs <100791052+nachitoxx6262@users.noreply.github.com>
2023-06-01 14:24:38 -0300

    add enviroment variable

--- db.py
+++ db.py
@@ -2,9 +2,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
+db_user = os.environ.get("USER")
+db_password = os.environ.get("PASSWORD")
+db_host  = os.environ.get("HOST")
 # Configuración de la base de datos
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:admin@localhost/hotelpy"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{db_user}:{db_password}@{db_host}/hotelpy"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
